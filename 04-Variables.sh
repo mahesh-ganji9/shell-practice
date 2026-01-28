799cb873da8e4e6ec1eaf1eb4d9fc0233d573069
mahesh <mahesh050917@gmail.com>
2026-01-28 13:12:45 +0530

    updated special variables

---
+++ 04-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: How $PERSON2 , How are you ?"
+echo "$PERSON2:: Hello $PERSON1 , I am good . How are you doing ?"
+echo "$PERSON1:: I am good , What are you Doing ? "
+echo "$PERSON2:: I am learning devops"
