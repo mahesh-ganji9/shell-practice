7784e355f7ee1668837386d6200c0dcce9f3196f
mahesh <mahesh050917@gmail.com>
2026-01-28 16:23:39 +0530

    conditions v5

--- 09.Conditions.sh
+++ 09.Conditions.sh
@@ -4,7 +4,7 @@ Num=$1
 
 if [ $Num -gt 20 ]; then
     echo "Given Number: $Num is  greater than 20"
-elif [$Num -eq 20]; then
+elif [ $Num -eq 20 ]; then
     echo "Given Number: $Num is equal to 20"
 else
     echo "Given Number: $Num is  less than 20"
