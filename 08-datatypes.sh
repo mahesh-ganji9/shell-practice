8d8843b4343e3e5c073052f63bca390227450f47
mahesh <mahesh050917@gmail.com>
2026-01-28 14:27:48 +0530

    data types v2

--- 08-datatypes.sh
+++ 08-datatypes.sh
@@ -6,3 +6,12 @@ NUM2=30
 SUM=$(($NUM1+$NUM2))
 
 echo "SUM is $SUM"
+
+#Arrays
+
+Fruits=("Apple" "Orange" "Banana")
+
+echo "Fruits are: ${Fruits[@]}"
+echo "First Fruit is : ${Fruits[0]}"
+echo "Second Fruit is : ${Fruits[1]}"
+echo "Third Fruit is : ${Fruits[2]}"
