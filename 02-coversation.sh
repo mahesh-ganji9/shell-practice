a1174875ed973868e87a2eb5d565d324c91a3d7a
mahesh <mahesh050917@gmail.com>
2026-01-28 12:48:30 +0530

    conversation file

---
+++ 02-coversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Mahesh:: How Suresh , How are you ?"
+echo "Suresh:: Hello Mahesh , I am good . How are you doing ?"
+echo "Mahesh:: I am good , What are you Doing ? "
+echo "Suresh:: I am learning devops"
