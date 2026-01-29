0041069b68075e79ec5aa12885f6b511499946d0
mahesh <mahesh050917@gmail.com>
2026-01-30 04:47:01 +0530

    installation script v4

---
+++ 11.installations_v1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+Userid=$(id -u)
+
+if [ $Userid -ne 0 ];then
+       
+       echo "please run the script with root user access"
+       exit 1
+   fi
+    
+echo "Installing Nginx"
+dnf install nginxx -y
+
+if [ $? -ne 0 ]; then
+   echo "Installing nginx...Failure"
+   
+else
+   echo "Installing nginx....Success"
+fi
