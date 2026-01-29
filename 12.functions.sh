d56d7ee2bac0fac5a781ae0f7d712097bc03c38d
mahesh <mahesh050917@gmail.com>
2026-01-30 05:02:41 +0530

    functions

---
+++ 12.functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+Userid=$(id -u)
+
+if [ $Userid -ne 0 ];then
+       
+       echo "please run the script with root user access"
+       exit 1
+   fi
+
+VALIDATE() {
+    if [ $? -ne 0 ]; then
+   echo "$2...Failure"
+else
+   echo "$2....Success"
+fi
+}
+    
+dnf install nginx -y
+VALIDATE $? "Installing Nginx" 
+
+dnf install htop -y
+VALIDATE $? "Installing htop" 
+
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs"" 
