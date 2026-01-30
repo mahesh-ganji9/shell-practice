77d34d2fb042a07be09cef681279f167bfbff9ff
mahesh <mahesh050917@gmail.com>
2026-01-30 05:34:29 +0530

    loops.sh_v1

---
+++ 16.loops.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+Userid=$(id -u)
+LOG_FOLDER="/var/log/shell-script"
+LOG_File="/var/log/shell-script/$0.log"
+
+if [ $Userid -ne 0 ];then
+       
+       echo "please run the script with root user access" | tee -a $LOG_File
+       exit 1
+   fi
+
+mkdir -p $LOG_FOLDER
+
+VALIDATE() {
+    if [ $? -ne 0 ]; then
+   echo "$2...Failure" | tee -a $LOG_File
+else
+   echo "$2....Success" | tee -a $LOG_File
+   fi
+}
+    
+for package in $@
+  do 
+     dnf install $package -y | tee -a $LOG_File
+     VALIDATE $? "$package Installation"
+  done
