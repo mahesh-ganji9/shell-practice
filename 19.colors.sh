cc7b3e76a76e87e75da367efa20f9109d700b8c8
mahesh <mahesh050917@gmail.com>
2026-01-30 06:11:21 +0530

    colors

---
+++ 19.colors.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+Userid=$(id -u)
+LOG_FOLDER="/var/log/shell-script"
+LOG_File="/var/log/shell-script/$0.log"
+
+if [ $Userid -ne 0 ];then
+       
+       echo -e "$R please run the script with root user access" | tee -a $LOG_File
+       exit 1
+   fi
+
+mkdir -p $LOG_FOLDER
+
+VALIDATE() {
+    if [ $? -ne 0 ]; then
+   echo "$2...Failure" | tee -a $LOG_File
+   
+else
+   echo "$2....Success" | tee -a $LOG_File
+   fi
+}
+    
+for package in $@
+  do 
+     dnf list installed $package &>>$LOG_File
+     if [ $? -ne 0 ]; then
+     echo "$package not installed ,installing now"
+     dnf install $package -y &>>$LOG_File
+     VALIDATE $? "$package Installation"
+    else
+      echo "$package already installed ,skipping"
+      fi
+  done
