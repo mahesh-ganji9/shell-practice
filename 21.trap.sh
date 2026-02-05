87155758d7fc312d5287148b5ac68fd11929be64
mahesh <mahesh050917@gmail.com>
2026-02-05 11:50:27 +0530

    trap commands

--- 21.trap.sh
+++ 21.trap.sh
@@ -1,9 +1,20 @@
-#!/bin/bash
+# #!/bin/bash
 
-trap "echo Finished the script" EXIT
-i=1
-while [ $i -le 5 ]
+# trap "echo Finished the script" EXIT
+# i=1
+# while [ $i -le 5 ]
+#  do
+#    i=$(($i+1))
+#    echo $i
+# done
+
+
+/bin/bash
+
+ trap "echo when press CTRL+C it will throw this" SIGINT
+ i=1
  do
    i=$(($i+1))
-   echo $i
-done
+   sleep 60
+echo $i
+ done
