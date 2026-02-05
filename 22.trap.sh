af0efb6f9ad1458ca55552d292f79809a20c54e4
mahesh <mahesh050917@gmail.com>
2026-02-05 12:53:26 +0530

    trap commands

--- 22.trap.sh
+++ 22.trap.sh
@@ -1,16 +1,11 @@
-# #!/bin/bash
-
-# set -e #ERR
-# trap 'echo "There is an error $LINENO, command: $BASH_COMMAND"' ERR
-
-# echo "This is Mahesh"
-# echo "This is Keeru"
-# dfff -h
-# echo "No error"
-
 #!/bin/bash
-set -e
-trap 'echo "There is is error $LINENO ,command: $BASH_COMMAND"' ERR
 
-echo "This Mahesh & Keeru"
-echooo
+set -e #ERR
+trap 'echo "There is an error $LINENO, command: $BASH_COMMAND"' ERR
+
+echo "This is Mahesh"
+echo "This is Keeru"
+dfff -h
+echo "No error"
+
+
