51b32e299fe7aa427eac8290e825d07cb6799574
mahesh <mahesh050917@gmail.com>
2026-02-06 21:19:03 +0530

    delete older files 14 days

--- 25.DeleteOlderFiles.sh
+++ 25.DeleteOlderFiles.sh
@@ -14,19 +14,17 @@ if [ -d $LOG_DIR ] ; then
 fi
 
 Files_to_Delete=$(find $LOG_DIR -name "*.log" -mtime +14)
-#echo $Files_to_Delete
 
-# if [ -z "$Files" ]; then
-#   echo -e "$Y There are no Files to delete older than 14 days"
-#   exit
-#   else
-#     for File in $Files
-#     do
-#     rm $File
-#     echo "Deleted file $File"
-#     done
-# fi
 
-while IFS= read -r line; do
-   echo $line 
-done <<< $Files_to_Delete 
+if [ -z "$Files_to_Delete" ]; then
+  echo -e "$Y There are no Files to delete older than 14 days"
+  
+  else
+    while IFS= read -r filepath; do
+         echo "Deleting file: $filepath"
+         rm -f $filepath
+         echo "Deleted file: $filepath"
+     done <<< $Files_to_Delete
+fi
+
+ 
