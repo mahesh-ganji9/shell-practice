1adac60f5351a2fccec906dd95225a165806e88e
mahesh <mahesh050917@gmail.com>
2026-02-08 06:52:27 +0530

    backup older

--- 28.disk_usage.sh
+++ 28.disk_usage.sh
@@ -5,8 +5,19 @@ Y="\e[33m"
 N="\e[0m"
 Userid=$(id -u)
 
-Threshold=60
 
-USAGE=$(df -hT | awk '{print $6}' | cut -d "%" -f1)
+Threshold=3
 
 echo $USAGE
+
+While IFS= read -r line
+do
+   USAGE=$(df -hT | grep -v Filesystem | awk '{print $6}'| cut -d "%" -f1)
+   PARTITON=$(df -hT | grep -v Filesystem | awk '{print $7}')
+   if [ $USAGE -gt $Threshold ]; then
+       
+       Message+="High Disk usage on $PARTITION:$USAGE"
+
+done
+
+  echo "$Message"
