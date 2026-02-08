e2ce246abcd4305af71bca936af4e2f625304885
mahesh <mahesh050917@gmail.com>
2026-02-08 06:57:22 +0530

    backup older

--- 28.disk_usage.sh
+++ 28.disk_usage.sh
@@ -4,13 +4,12 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 Userid=$(id -u)
+Message=""
 
 
 Threshold=3
 
-echo $USAGE
-
-While IFS= read -r line
+while IFS= read -r line
 do
    USAGE=$(df -hT | grep -v Filesystem | awk '{print $6}'| cut -d "%" -f1)
    PARTITON=$(df -hT | grep -v Filesystem | awk '{print $7}')
