efb7c93a268dac46b8577bd00f7d3312c1e84775
mahesh <mahesh050917@gmail.com>
2026-02-09 15:05:07 +0530

    backup older

--- 29.mail.sh
+++ 29.mail.sh
@@ -5,7 +5,7 @@ MESSAGE_BODY=$4
 TEAM=$5
 
 
-FINAL_MESSAGE=$(sed -e "s/TEAM/$TEAM/g" -e "s/Server_IP/$IP_ADDRESS/g" -e "s/MESSAGE/$MESSAGE/g" template.html)
+FINAL_MESSAGE=$(sed -e "s/TEAM/$TEAM/g" -e "s/Server_IP/$IP_ADDRESS/g" -e "s/MESSAGE/$MESSAGE_BODY/g" template.html)
 
 
 { echo "$FROM_ADDRESS";
