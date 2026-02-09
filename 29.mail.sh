950cdd94ff02f8941ed74eba5095766ee3723cde
mahesh <mahesh050917@gmail.com>
2026-02-09 13:56:38 +0530

    backup older

--- 29.mail.sh
+++ 29.mail.sh
@@ -3,7 +3,6 @@ SUBJECT=$2
 MESSAGE_BODY=$3
 SERVER_IP=$4
 TO_TEAM=$5
-SERVER_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed -e 's/[]\/$*.^[]/\\&/g')
 
 MESSAGE=$(sed -e "s/T0_TEAM/$TO_TEAM/g" -e "s/SERVER_IP/$SERVER_IP/g")
