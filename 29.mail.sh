47383b6dcdc775abb93dec9a969d06b8b6e948c5
mahesh <mahesh050917@gmail.com>
2026-02-09 17:09:00 +0530

    backup older

--- 29.mail.sh
+++ 29.mail.sh
@@ -7,7 +7,8 @@ IP_ADDRESS=$6
 
 # FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed -e 's/[]\/$*.^[]/\\&/g')
 FINAL_MESSAGE_BODY="<table border='1' cellpadding='5' cellspacing='0'> $MESSAGE </table>"
-FINAL_MESSAGE=$(sed -e "s/TEAM/$TEAM/g" -e "s/SERVER_IP/$IP_ADDRESS/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g" template.html)
+FINAL_MESSAGE=$(sed -e "s|TEAM|$TEAM|g" -e "s|SERVER_IP|$IP_ADDRESS|g" -e "s|MESSAGE|$FINAL_MESSAGE_BODY|g" template.html)
+# FINAL_MESSAGE=$(sed -e "s/TEAM/$TEAM/g" -e "s/SERVER_IP/$IP_ADDRESS/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g" template.html)
 
 {
 echo "To: $TO_ADDRESS"
