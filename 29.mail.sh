06ffeab3996887e58ff314e75957382f3af41b15
mahesh <mahesh050917@gmail.com>
2026-02-09 17:06:24 +0530

    backup older

--- 29.mail.sh
+++ 29.mail.sh
@@ -5,7 +5,8 @@ MESSAGE_BODY=$4
 TEAM=$5
 IP_ADDRESS=$6
 
-FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed -e 's/[]\/$*.^[]/\\&/g')
+# FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed -e 's/[]\/$*.^[]/\\&/g')
+FINAL_MESSAGE_BODY="<table border='1' cellpadding='5' cellspacing='0'> $MESSAGE </table>"
 FINAL_MESSAGE=$(sed -e "s/TEAM/$TEAM/g" -e "s/SERVER_IP/$IP_ADDRESS/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g" template.html)
 
 {
