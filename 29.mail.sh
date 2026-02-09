4a74d5397a8a08e6a587480a2bd1b564a809403a
mahesh <mahesh050917@gmail.com>
2026-02-09 13:50:44 +0530

    backup older

--- 29.mail.sh
+++ 29.mail.sh
@@ -6,12 +6,12 @@ TO_TEAM=$5
 SERVER_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed -e 's/[]\/$*.^[]/\\&/g')
 
-MESSAGE=$(sed -e "s/T0_TEAM/$TO_TEAM/g" -e "s/SERVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g")
+MESSAGE=$(sed -e "s/T0_TEAM/$TO_TEAM/g" -e "s/SERVER_IP/$SERVER_IP/g")
 
 {
 echo "To: $TO_ADDRESS"
 echo "Subject: $SUBJECT"
 echo "Content-Type: text/html"
 echo ""
-echo "$MESSAGE"
+echo "Test"
 } | msmtp "mahesh9.storage@gmail.com"
